a1d81a27154a1f0f34641fd5e3e519643e20846e
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-01-25 18:15:25 +0700

    Disable Gravity for simulate handle

---
+++ Assets/Scripts/Handle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Handle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().useGravity = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
