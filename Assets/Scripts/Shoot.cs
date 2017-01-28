98a05c4ae409f31ebf53ebe9a9dc4fb82202539a
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-01-28 18:19:34 +0700

    Shooting Script

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().AddForce(0, 10, -55, ForceMode.Impulse);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
